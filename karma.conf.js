ac1f51262ee5cae399051d57994ef38d3792668d
Christopher Lenz <lenz@mediatis.de>
2016-04-19 10:50:38 +0200

    Update Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,59 +1,80 @@
 // Karma configuration
-// Generated on Sat May 23 2015 00:26:01 GMT+0200 (CEST)
+// Generated on Tue Apr 19 2016 10:44:24 GMT+0200 (CEST)
 
 module.exports = function(config) {
   config.set({
+
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
+
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['browserify', 'jasmine'],
+
 
     // list of files / patterns to load in the browser
     files: [
       'src/**/*.spec.ts'
     ],
 
+
     // list of files to exclude
     exclude: [
     ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'src/**/*.ts': 'browserify'
+    },
+
+
+    // browserify configuration
+    // options: https://github.com/substack/node-browserify#browserifyfiles--opts
+    browserify: {
+      debug: true,
+      plugin: [
+        ['tsify']
+      ]
+    },
+
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
+
     // web server port
     port: 9876,
 
+
     // enable / disable colors in the output (reporters and logs)
     colors: true,
+
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
+
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
 
-    preprocessors: {
-      'src/**/*.ts': 'browserify'
-    },
-
-    browserify: {
-      debug: true,
-      plugin: [
-        ['tsify', {target: 'es5'}]
-      ]
-    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
-  });
-};
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
